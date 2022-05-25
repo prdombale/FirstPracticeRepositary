9cc23d86f4320efc09ede83b984f198fc28fba68
Amol.Dombale <amoldombale2035@gmail.com>
2022-05-25 17:55:28 +0530

    Summary Write
    Discription Write

---
+++ StartProject/src/projectStart123/ClassABC.java
@@ -0,0 +1,10 @@
+package projectStart123;
+
+public class ClassABC {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome");
+System.out.println("Welcome to homePage");
+	}
+
+}
